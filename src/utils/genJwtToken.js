47fc95efff641efc4bc0f9c4e36062ee2723b4ee
amangusain302 <amangusain302@gmail.com>
2023-05-25 23:35:59 +0530

    complete machine task

--- src/utils/genJwtToken.js
+++ src/utils/genJwtToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = genJWTtoken = (data) => {
     console.log(data._id);
     console.log(process.env.JWT_SECRET);
-    return jwt.sign({ _id : data._id }, "amangusain", {
+    return jwt.sign({ _id : data._id }, process.env.JWT_SECRET, {
         expiresIn: "15d"
     })
 }
